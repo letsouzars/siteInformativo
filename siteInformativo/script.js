dbfaf667d651fd558101a68dcadc1fe52f27ccd9
Leticia Souza <133662327+letsouzars@users.noreply.github.com>
2024-09-30 11:31:41 -0300

    Add files via upload

---
+++ siteInformativo/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const fadeElements = document.querySelectorAll('.fade-in');
+    fadeElements.forEach((el) => observer.observe(el));
+});
+ 
